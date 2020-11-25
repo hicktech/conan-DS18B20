84adb8dfd7392a0346bd6bd6a83e9657a764149f
John Wass <jwass3@gmail.com>
2020-11-25 15:05:40 -0500

    initial import

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+from conans import ConanFile
+
+
+class ParticlePackage(ConanFile):
+    name = 'DS18B20'
+    version = 'f489e4a'
+    url = 'https://github.com/hicktech/conan-DS18B20'
+    repo_url = 'https://github.com/tomdeboer/SparkCoreDallasTemperature.git'
+    generators = 'cmake'
+    settings = []
+    requires = []
+
+    def package(self):
+        self.copy('*.c*', dst='src')
+        self.copy('*.h*', dst='include')
+
+    def source(self):
+        self.run(f'git clone {self.repo_url} .')
+        self.run(f'git checkout {self.version}')
+
+    def package_info(self):
+        self.cpp_info.srcdirs = ['src']
