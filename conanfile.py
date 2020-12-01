ee3658a9a9e361f3d2cf88f51fefcb36ab2b4208
John Wass <jwass3@gmail.com>
2020-12-01 16:39:35 -0500

    add dep and src path

--- conanfile.py
+++ conanfile.py
@@ -8,11 +8,11 @@ class ParticlePackage(ConanFile):
     repo_url = 'https://github.com/tomdeboer/SparkCoreDallasTemperature.git'
     generators = 'cmake'
     settings = []
-    requires = []
+    requires = ['OneWire/c5007f4@hicktech/stable']
 
     def package(self):
-        self.copy('*.c*', dst='src')
-        self.copy('*.h*', dst='include')
+        self.copy('*.c*', dst='src', src='src')
+        self.copy('*.h*', dst='include', src='src')
 
     def source(self):
         self.run(f'git clone {self.repo_url} .')
